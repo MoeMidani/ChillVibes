651c1cf3be7dc1c01d498f40c92faf728c247234
Jennifer_Simond <jenniasimond@gmail.com>
2023-11-08 09:00:15 -0500

    chapter 5

--- CLIStoryTemplate/Chapter 5/Chapter5.swift
+++ CLIStoryTemplate/Chapter 5/Chapter5.swift
@@ -6,5 +6,5 @@
 import Foundation
 
 func chapterFive() {
-    // Your portion of the story goes here
+ print("\(characterName) walked a few miles and found his way to the old duck pond near the endge of the forest, and there was grandpa George. \(characterName) was ran and gave their grandpa a big hug. Granpa George told him how pround he was of \(characterName) for making their way through the forrest. he and presented \(characterName) with a new compass and pair of binoculors")
 }
